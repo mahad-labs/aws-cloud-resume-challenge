eec8dadde3c910b391af8b48868a22ee3726c631
mahad-labs <55285307+mahad-labs@users.noreply.github.com>
2024-03-05 14:17:12 +0000

    Update index.js

--- website/index.js
+++ website/index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter()
 {
-    let response = await fetch("https://jfcdziokjxuisfv6almamx3wum0gqyjn.lambda-url.us-east-1.on.aws/")
+    let response = await fetch("https://usbqqwfdcxinbzkmnzdyfiz3ey0lvtbo.lambda-url.us-east-1.on.aws/")
     let data = await response.json();
     counter.innerHTML   = ` Views: ${data}`;
 }
